99a2317fda647f01877ba8ef6133343384584e7f
Villiruusu <javillir@gmail.com>
2019-10-16 17:08:39 +0300

    Tehty node-sovellus, asennettu express ja nodemon, luotu /api/persons ja /info routet

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const app = express()
+
+const persons = [
+  {
+    name: "Arto Hellas",
+    number: "040-1234567",
+    id: 1
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: 2
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+  const personsLength = Number(persons.length)
+  const date = new Date()
+  response.send(`<p>Phonebook has info for ${personsLength} people</p> <p> ${date}</p>`)
+})
+
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
