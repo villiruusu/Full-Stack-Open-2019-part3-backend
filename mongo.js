df1d2c3ff313be49d2bf490d72099699991429a8
Villiruusu <javillir@gmail.com>
2019-10-24 12:14:10 +0300

    asennettu mongoose ja tehty tehtava 3.12 eli mongo.js-tiedosto

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if ( process.argv.length<3 ) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://villiruusu:${password}@cluster0-tt2jd.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({ 
+    name: name,
+    number: number,
+})
+
+
+if (name && number) {
+    person.save().then(response => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`);
+    mongoose.connection.close();
+    })
+} else if (!name && !number) {
+    Person.find({}).then(result => {
+        console.log("phonebook:")
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
